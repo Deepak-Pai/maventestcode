b7ff0d34e27ac90aa31328a5771d5756d11f1718
deepa <deepa@LAPTOP-6RHKGB6P>
2019-08-25 15:46:42 +0530

    TestNG Addition

---
+++ maventest/src/main/java/fristtestngpackage/FirsttestNGfile.java
@@ -0,0 +1,35 @@
+package fristtestngpackage;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+
+public class FirsttestNGfile {
+ 
+	 public String baseUrl = "http://ec2-3-17-109-74.us-east-2.compute.amazonaws.com/index.php";
+	    String driverPath = "C:\\Users\\deepa\\eclipse-workspace\\PHP-App-Testing\\chromedriver.exe";
+	    public WebDriver driver ; 
+	    
+	    
+	
+	
+	@Test
+  public void VerifyAboutPageTitle() {
+  
+		System.out.println("launching chrome browser"); 
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
+		driver.findElementById("About Us").click();
+		String AboutCont="This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		String ActualCont = driver.findElementById("PID-ab2-pg").getText();
+		Assert.assertEquals(AboutCont, ActualCont);
+		driver.close();
+					
+	}
+}
--- maventest/src/test/java/com/org/maventest/maventest/verify_about.java
+++ maventest/src/test/java/com/org/maventest/maventest/verify_about.java
@@ -4,14 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class verify_about {
 
 	//public static String chromeDriverPath = "/usr/local/bin/chromedriver";
-public static String chromeDriverPath = "C:\\Users\\deepa\\eclipse-workspace\\PHP-App-Testing\\chromedriver.exe";
+public static String chromeDriverPath = "C:\\Users\\deepa\\eclipse-workspace"
+		+ "\\PHP-App-Testing\\chromedriver.exe";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("http://ec2-3-17-109-75.us-east-2.compute.amazonaws.com/index.php");
+		driver.get("http://ec2-3-17-109-74.us-east-2.compute.amazonaws.com/index.php");
 		String x = driver.getTitle();
 		System.out.println(x);
 		String z = null;
