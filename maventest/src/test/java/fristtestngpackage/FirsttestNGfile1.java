1b3cb95d137f8e74a1eac620337c5082216c30fe
deepa <deepa@LAPTOP-6RHKGB6P>
2019-08-25 16:04:24 +0530

    Added try catch

--- maventest/src/test/java/fristtestngpackage/FirsttestNGfile1.java
+++ maventest/src/test/java/fristtestngpackage/FirsttestNGfile1.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 
 public class FirsttestNGfile1 {
  
-	 public String baseUrl = "http://ec2-3-17-109-75.us-east-2.compute.amazonaws.com/index.php";
+	 public String baseUrl = "http://ec2-3-17-109-74.us-east-2.compute.amazonaws.com/index.php";
 	    String driverPath = "C:\\Users\\deepa\\eclipse-workspace\\PHP-App-Testing\\chromedriver.exe";
 	    public WebDriver driver ; 
 	    
@@ -24,10 +24,16 @@ public class FirsttestNGfile1 {
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(baseUrl);
-		driver.findElementById("About Us").click();
-		String AboutCont="This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-		String ActualCont = driver.findElementById("PID-ab2-pg").getText();
-		Assert.assertEquals(AboutCont, ActualCont);
+		
+		try {
+			driver.findElementById("About Us").click();
+			String AboutCont="This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+			String ActualCont = driver.findElementById("PID-ab2-pg").getText();
+			Assert.assertEquals(AboutCont, ActualCont);
+		} catch (Exception e) {
+			System.out.println("Invalid Element");
+		}
+
 		driver.close();
 					
 	}
