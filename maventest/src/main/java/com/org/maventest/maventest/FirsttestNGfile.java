f0c68b0d0ca71fb1a2bda80ce15eced238f28dc7
deepa <deepa@LAPTOP-6RHKGB6P>
2019-08-25 17:05:33 +0530

    newfiles

--- maventest/src/main/java/com/org/maventest/maventest/FirsttestNGfile.java
+++ maventest/src/main/java/com/org/maventest/maventest/FirsttestNGfile.java
@@ -1,4 +1,4 @@
-package fristtestngpackage;
+package com.org.maventest.maventest;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
@@ -10,7 +10,9 @@ import org.testng.Assert;
 public class FirsttestNGfile {
  
 	 public String baseUrl = "http://ec2-3-17-109-74.us-east-2.compute.amazonaws.com/index.php";
-	    String driverPath = "C:\\Users\\deepa\\eclipse-workspace\\PHP-App-Testing\\chromedriver.exe";
+	    //String driverPath = "C:\\Users\\deepa\\eclipse-workspace\\PHP-App-Testing\\chromedriver.exe";
+	    String driverPath = "/usr/local/bin/chromedriver";
+	    
 	    public WebDriver driver ; 
 	    
 	    
@@ -25,16 +27,22 @@ public class FirsttestNGfile {
 		driver.manage().window().maximize();
 		driver.get(baseUrl);
 		
-		try {
+try {
 			driver.findElementById("About Us").click();
 			String AboutCont="This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 			String ActualCont = driver.findElementById("PID-ab2-pg").getText();
 			Assert.assertEquals(AboutCont, ActualCont);
 		} catch (Exception e) {
 			System.out.println("Invalid Element");
+		Assert.assertFalse(false);
 		}
-
-		driver.close();
-					
-	}
+/*
+		
+		driver.findElementById("About Us").click();
+		String AboutCont="This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		String ActualCont = driver.findElementById("PID-ab2-pg").getText();
+		Assert.assertEquals(AboutCont, ActualCont);
+		driver.close();		
+	} */
+}
 }
